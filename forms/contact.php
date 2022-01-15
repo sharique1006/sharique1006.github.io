395c0cb27d06c46a1ab2e561f4b19641babcc0fa
Sharique Shamim <64735973+sharique1006@users.noreply.github.com>
2022-01-15 20:30:47 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,58 +1,41 @@
-<?php 
-echo "Hello-1"
-$errors = '';
-$myemail = 'shariqueshamim001@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) ||
-   empty($_POST['subject']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-echo "Hello-2"
-
-$name = $_POST['name']; 
-$email_address = $_POST['email'];
-$subject = $_POST['subject'] 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-echo "Hello-3"
-
-if( empty($errors))
-{
-  $to = $myemail; 
-  $email_subject = "Contact form submission: $name";
-  $email_body = "You have received a new message. ".
-  " Here are the details:\n Name: $name \n Email: $email_address \n Subject : $subject \n Message \n $message"; 
-  
-  $headers = "From: $myemail\n"; 
-  $headers .= "Reply-To: $email_address";
-  echo "Hello-4"
-  
-  mail($to,$email_subject,$email_body,$headers);
-  //redirect to the 'thank you' page
-  // header('Location: contact-form-thank-you.html');
-  echo "Thank you for contacting!"
-} 
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-  <title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
 <?php
-echo nl2br($errors);
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'contact@example.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
 ?>
-
-
-</body>
-</html>
